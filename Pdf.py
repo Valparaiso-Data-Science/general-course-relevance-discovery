b84f2eba3d39a081c9242a8920c3047e1a6ddb1d
Cody Packer <codypacker@valpo.edu>
2019-05-19 17:09:07 -0500

    Comments for things to do at the top

--- Pdf.py
+++ Pdf.py
@@ -1,3 +1,16 @@
+#THINGS TO ADD
+    #Once we regex for the "MATH 240" stuff, see if there is a way to check what the next line is
+    #we want to see if the next letter is a capital (small detail but will remove a lot of mistake)
+    #This would solve some of the cases in which prerequisites are listed in the description, they dont typically have a space or capital letter so we could keep them in the description instead of thinking it is a new class
+
+    #Need to find a way to make an end case, some schools have classes split between departments,
+    #we don't want to record every page between Arts & Sci to Nursing, we need a way to know this is extractText
+    #easiest way I can think of is over x over the average course description (they only get 200 characters big or something around that, right?)
+
+    #Output to a csv or put into a data frame,
+    #Need to be able to access the indexs so everytime we see a new class, we check if we already documented stuff about it
+    #If it is a duplicate we want to append to end of the dataframe so we don't have duplicates
+
 import PyPDF2   #PDF Reader
 import re       #Regex
 import csv      #CSV
@@ -33,6 +46,7 @@ for page in range(pdf.numPages):
                 #and (not re.search('ANNOUNCEMENTS', line)) and (not re.search('scholarship', line)))
             if newClass:
                 #First loop combines nothing, but after that details is an array of details about the class
+                #| is for conveince so we see what each detail is being joined into output
                 output = '|'.join(details)
                 file.write(output+"\n")
                 #Once you write the class details, forget it
