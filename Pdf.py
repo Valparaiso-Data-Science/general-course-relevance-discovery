cdfa9ad2d91c59d3027f630aa941a96480795568
Cody Packer <codypacker@valpo.edu>
2019-05-19 17:00:39 -0500

    Implemented Regex to define the start of a new class (Everytime regex becomes true start a new class)

--- Pdf.py
+++ Pdf.py
@@ -1,31 +1,48 @@
-import PyPDF2
-import re
+import PyPDF2   #PDF Reader
+import re       #Regex
+import csv      #CSV
 
+#Open PDF and convert to a string based on new lines
+#(PyPDF acknowledges that pdf reading newlines is kinda wonky, natural)
 
-#PDF AND OUTPUT PREP
-#OUTPUT WILL BE EITHER ALL CLASSES OR ALL DATA SCIENCE RELAVENT CLASSES (DEPENDING HOW WE CAN DO THIS)
+#Cody uses this one for conveincience
+#pdfFileObj = open("ValpoTest.pdf", 'rb')
+
+#Nate uses this one cause first one didnt work at first
 filePath = input("Please input the file: ")
 pdfFileObj = open(filePath, 'rb')
+
 pdf = PyPDF2.PdfFileReader(pdfFileObj)
+
 file = open("Output.txt","w")
+start = False   #Variable used to avoid writing everything before first class is found
+details = []    #Array of detail
 
 #Go through every page
-#for page in range(pdfReader.numPages):
-pageObj = pdf.getPage(0)
+for page in range(pdf.numPages):
+    pageObj = pdf.getPage(page)
+    text = pageObj.extractText()
+    updated = text.split("\n")
+    updated = filter(None, updated)
+    #Go through every line
+    for line in updated:
+        if line != " ":
+            #print(repr(line))
+            #See if the current line is a header (MATH 240)
+            newClass = bool(re.search("[A-Z]{3,5}\s[0-9]{3,4}", line))
+                #and (not re.search('ANNOUNCEMENTS', line)) and (not re.search('scholarship', line)))
+            if newClass:
+                #First loop combines nothing, but after that details is an array of details about the class
+                output = '|'.join(details)
+                file.write(output+"\n")
+                #Once you write the class details, forget it
+                details.clear()
+                #Once you find a class header, set to true
+                start = True
+                details.append(line)
+                #Once one class header is found, you can start recording information
+            elif start:
+                details.append(line)
 
-text = pageObj.extractText()
-
-#this regex grabs 3-4 capital letters, a space, and 3-4 numbers
-classCodeRegex = "[A-Z]{3,4}\s[0-9]{3,4}"
-
-
-
-for line in text:
-    if re.search(classCodeRegex, line):
-        print(line)
-print(re.findall(classCodeRegex, text))
-
-
-file.write(pageObj.extractText())
 file.close()
 pdfFileObj.close()
