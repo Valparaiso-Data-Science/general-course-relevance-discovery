1a5fe021e46e1ccefe6d23e86b3023d90a4acb1f
Cody Packer <cody.packer@valpo.edu>
2019-05-20 16:17:39 -0500

    More complex regex in pdf.py (checks next line) and updated README with a TODO list

--- Pdf.py
+++ Pdf.py
@@ -1,29 +1,15 @@
-#THINGS TO ADD
-    #Once we regex for the "MATH 240" stuff, see if there is a way to check what the next line is
-    #we want to see if the next letter is a capital (small detail but will remove a lot of mistake)
-    #This would solve some of the cases in which prerequisites are listed in the description, they dont typically have a space or capital letter so we could keep them in the description instead of thinking it is a new class
-
-    #Need to find a way to make an end case, some schools have classes split between departments,
-    #we don't want to record every page between Arts & Sci to Nursing, we need a way to know this is extractText
-    #easiest way I can think of is over x over the average course description (they only get 200 characters big or something around that, right?)
-
-    #Output to a csv or put into a data frame,
-    #Need to be able to access the indexs so everytime we see a new class, we check if we already documented stuff about it
-    #If it is a duplicate we want to append to end of the dataframe so we don't have duplicates
-
 import PyPDF2   #PDF Reader
 import re       #Regex
 import csv      #CSV
 
 #Open PDF and convert to a string based on new lines
 #(PyPDF acknowledges that pdf reading newlines is kinda wonky, natural)
-
 #Cody uses this one for conveincience
-#pdfFileObj = open("ValpoTest.pdf", 'rb')
+pdfFileObj = open("ValpoTest.pdf", 'rb')
 
 #Nate uses this one cause first one didnt work at first
-filePath = input("Please input the file: ")
-pdfFileObj = open(filePath, 'rb')
+#filePath = input("Please input the file: ")
+#pdfFileObj = open(filePath, 'rb')
 
 pdf = PyPDF2.PdfFileReader(pdfFileObj)
 
@@ -36,14 +22,16 @@ for page in range(pdf.numPages):
     pageObj = pdf.getPage(page)
     text = pageObj.extractText()
     updated = text.split("\n")
-    updated = filter(None, updated)
+    #updated = filter(None, updated)
     #Go through every line
-    for line in updated:
-        if line != " ":
+    for line in range(len(updated)):
+        if updated[line] != " ":
             #print(repr(line))
             #See if the current line is a header (MATH 240)
-            newClass = bool(re.search("[A-Z]{3,5}\s[0-9]{3,4}", line))
-                #and (not re.search('ANNOUNCEMENTS', line)) and (not re.search('scholarship', line)))
+            newClass = bool(re.search("[A-Z]{3,5}\s[0-9]{3,4}", updated[line]))
+            #TODO, the current 'updated' array has a bunch of empty strings, need to filter() them but revert back to an array to we can len(range()) it
+            if newClass and (line+2)<len(updated):
+                newClass = bool(re.search("^[A-Z]",updated[line+2]))
             if newClass:
                 #First loop combines nothing, but after that details is an array of details about the class
                 #| is for conveince so we see what each detail is being joined into output
@@ -53,10 +41,10 @@ for page in range(pdf.numPages):
                 details.clear()
                 #Once you find a class header, set to true
                 start = True
-                details.append(line)
+                details.append(updated[line])
                 #Once one class header is found, you can start recording information
             elif start:
-                details.append(line)
+                details.append(updated[line])
 
 file.close()
 pdfFileObj.close()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # general-course-relevance-discovery
 This repo is intended to house code that allows for data science topics discovery from university course catalogs
+
+Pdf.py TODO list
+  Need to get rid of newlinds in 'updated' array of lines because part of our regex includes checking the next line for a capital letter (Cody can explain) but if their is a space between we would have to if statement account for it, id rather not have to worry about spacings. I previously used filter() but I coulded range(len()) a filter object so if you can revert it back that would work nicely
+
+  Need to find an end case, not all courses end at the last page, we need to think of a way to differentiate courses from classes. Some split between fields/majors and then the last major ends but the catalog keeps rambling
+
+  Output to a file other than our current write to text
+
+  Either in Py or in the "CSV naive regex" we need to merge duplicate classes found (I think this will be easier in the CSV cleaning rather than the py stuff, py is getting kinda complex)
