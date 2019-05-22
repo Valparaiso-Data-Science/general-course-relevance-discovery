941bfaebd15682961661e2668bbfaac4592ab2a9
CodyPacker <cody.packer@valpo.edu>
2019-05-22 14:03:06 -0500

    parse.py comment cleaning

--- source/parse.py
+++ source/parse.py
@@ -3,7 +3,7 @@ import re       #Regex
 import csv      #CSV
 
 #Grab PDF and create outpute file
-pdfFileObj = open("ValpoUni.pdf", 'rb')
+pdfFileObj = open("../fullPDFs/ValpoUni.pdf", 'rb')
 pdf = PyPDF2.PdfFileReader(pdfFileObj)
 file = open("Output.txt","w")
 
@@ -38,7 +38,6 @@ for page in range(pdf.numPages):
 
             #Format 1
             if newClass and (line+7)<len(updated) and bool(updated[line+3] == " " and updated[line+5] == " "):
-                print("Attempt1: ",updated[line])
                 newClass = bool(updated[line+5] == " " and updated[line+6] == " " and (re.search("^[A-Z][a-z]",updated[line+7])) or (re.search("^[A-Z]\s",updated[line+7])))
                 #print(updated[line])
             else:
@@ -46,14 +45,12 @@ for page in range(pdf.numPages):
 
             #Format 2
             if newClass and (line+8)<len(updated) and bool(re.search("[A-z]",updated[line+3]) and updated[line+6] == " "):
-                print("Attempt2: ",updated[line])
                 newClass = bool(updated[line+4] == " " and updated[line+6] == " " and updated[line+7] == " " and (re.search("^[A-Z][a-z]",updated[line+8]) or re.search("^[A-Z]\s",updated[line+8])))
             else:
                 attempt2 = True
 
             #Format 3
             if newClass and (line+9)<len(updated) and (bool(re.search("-",updated[line+6])) or (re.search("-",updated[line+5]))):
-                print("Attempt3: ",updated[line])
                 newClass = bool(updated[line+8] == " " and updated[line+10] == " " and updated[line+11] == " " and (re.search("^[a-zA-z]",updated[line+12]) or re.search("^[A-Z]\s",updated[line+12])) or (updated[line+7] == " " and updated[line+8] == " " and updated[line+3] == " " and (re.search("^[a-zA-z]",updated[line+9]) or re.search("^[A-Z]\s",updated[line+9]))))
             else:
                 attempt3 = True
@@ -63,23 +60,9 @@ for page in range(pdf.numPages):
                 attempt1 = False
                 attempt2 = False
                 attempt3 = False
-            #if newClass and (line+8)<len(updated) and bool(updated[line+6]== " "):
-            #    if newClass and bool(re.search(".",updated[line+3])):
-            #        newClass = bool(re.search("^[A-Z][a-z]",updated[line+8]) or re.search("^[A-Z]\s",updated[line+8]))
-            #    else:
-            #        newClass = bool(re.search("^[A-Z][a-z]",updated[line+7]) or re.search("^[A-Z]\s",updated[line+7]))
-            #else:
-            #    newClass = False
 
-            #Check for Course descript to start with Capital letter and lowercase (or for an A and space)
-        #    if newClass and (line+9)<len(updated):
-        #        newClass = bool(re.search("^[A-Z][a-z]",updated[line+9]) or re.search("^[A-Z]\s",updated[line+9]))
-        #    else:
-        #        newClass = False
-#test
             #If you are a new class title:
             if newClass:
-                print("YESSSSSSSSS", updated[line])
                 #Combine everything between finding a class at the beginning and another at the end
                 output = '|'.join(details)
                 file.write(output+"\n\n")
@@ -97,4 +80,3 @@ for page in range(pdf.numPages):
 
 file.close()
 pdfFileObj.close()
-
