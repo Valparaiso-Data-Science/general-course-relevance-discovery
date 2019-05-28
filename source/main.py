a85602104bf654f6e851ac38117bca8fc1e58ac3
Nate <nathan.randle@valpo.edu>
2019-05-28 16:19:59 -0500

    Updated many comments and rid some bad code

--- source/main.py
+++ source/main.py
@@ -1,21 +1,36 @@
-from parseValpo import getClasses
 from parse import parse
 import PyPDF2
 from RD import findRelevant
 import json
 
+
+
+
 d = {}
-testing = True
-school = "Valpo2"
+testing = True #testing determines if we are working in the testPDF or the fill pdf 
+school = "Valpo2" #school is the filename of the pdf we want, make sure its in the directory you are working in
 
 filePath = "../%sPDFs/%s.pdf" % (("test" if testing else "full"), school)
 
 pdfFileObj = open(filePath, 'rb')
 doc = PyPDF2.PdfFileReader(pdfFileObj)
-#dictionary = getClasses(pdfFileObj)
-for i in doc.pages:
-    #print(i.extractText())
+
+"""
+@terry
+
+Currently this program runs on each page of the PDF object, and determines how
+to add to the dict after parsing the page. This is bad.
+Can you make it so that we just call parse() on a pdf object and it spits out
+a dict with all the classes.
+
+"""
+
+for i in doc.pages: #for each page in the document
+    #parse out the classes found
     newClasses = parse(i.extractText(),"[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}")
+    
+    #if the class is in the dict, append the class, 
+    #otherwise just create the class
     for classID in newClasses:
         if classID in d:
             d[classID] += newClasses[classID]
@@ -23,14 +38,20 @@ for i in doc.pages:
             d[classID] = newClasses[classID]
 pdfFileObj.close()
 
+
+
+x = findRelevant(d)
+with open("../output/output%s.csv" % school, "w") as f:
+    f.write('ClassID,Desc\n')
+    for key in x:
+        f.write('%s,"%s"\n'%(key,x[key]))
+        """
+#currently we 
 f= open("../output/output%s.json" % school, "w")
-
-
 json = json.dumps(findRelevant(d))
-
 f.write(json)
 f.close()
-
+"""
 
 
 #x = ["\n".join("{}\t{}".format(k, v) for k, v in dictionary.items())]
--- source/parse.py
+++ source/parse.py
@@ -1,33 +1,11 @@
 import re       #Regex
 import csv      #CSV
 
-
-#text is just some sample text to test parse() on
-text = '''
-ENT 100 Introduction to Entrepreneurship 3 Cr.
-An interdisciplinary survey course covering all dimensions of starting and operating a new business venture.
-Students will analyze and investigate the current trends and opportunities in entrepreneurship. Topics include
-entrepreneurship theory, opportunity recognition, the entrepreneurial process, entrepreneurship in a
-corporate context, the characteristics of entrepreneurs, market assessment, and entrepreneurial careers.
-
-ENT 200 Creativity and Innovation 3 Cr.
-An examination of the techniques and processes of innovation and creativity that lead to new business
-development as an independent new venture or in existing organizations. Exposure to techniques, concepts,
-and methods for managing the creative process in individual and group contexts with emphasis on
-accountability for creative quality. Lectures, experiential learning, discussions, and guest speakers.
-
-ENT 330 Business Planning and Venture Finance 3 Cr.
-A survey course focusing on development of business plans and raising capital. The components of a business
-plan are covered, including the research process required to gather necessary information. Sources of seed
-and growth capital are covered, as well as financial challenges faced by the entrepreneur. Students develop a
-comprehensive business plan that is presented to a panel of entrepreneurs and bankers.
-
-'''
-
-
-#parse is a function that, given a string of text, will pull out the class descriptions
+#parse is a function that, given a *string* of text, will pull out the class descriptions
 def parse(text, regex):
+    #clear the new lines
     text = text.replace("\n","")
+    
     #create a list of match objects that each contain the indices of the matches
     matches = list(re.finditer(regex, text))
 
@@ -38,28 +16,31 @@ def parse(text, regex):
     
     #loop through the match objects, enumerate makes it so the index is available
     for i,m in enumerate(matches):
-            #classId is set too the current matches entire string
+            
+            #classId is set too the current match's string
             classID = m.group(0)
-            #the start of the classes description starts with the END of the ClassID
+            #the start of the current class's description starts with the END of the ClassID
             startDesc = m.end()
             
-            
-            if i < lengthMatches-1: #if we are not on the last item
-                #the end of the description will be at the start of the next ClassID found
+            #if we are not on the last item,
+            #the end of the description will be at the start of the next ClassID found
+            #otherwise the description ends at the end of the text
+            if i < lengthMatches-1: 
                 endDesc = matches[i+1].start()
             else: 
-                #otherwise the description ends at the end of the text
                 endDesc = len(text)-1
-                
-                #if the class has not been found before, add it to the dict
-                #otherwise append the found descriptions together
+            """
+            @terry, as you can see this check is does twice, once on the current page
+            once after we are done finding the current page.
+            This is bad, see if there's a way to do this check as a function or
+            if there's a better way to do this
+            """
+            #if the class has not been found before, add it to the dict
+            #otherwise append the found descriptions together
             if classID not in result:
                 result[classID] = text[startDesc:endDesc]
             else:
                 result[classID] += text[startDesc:endDesc]
-                #print(result[classID])
-            #print(classID + ": " + str(startDesc) + "," + str(endDesc))
-            #print(text[startDesc:endDesc])
     return result
 
 
