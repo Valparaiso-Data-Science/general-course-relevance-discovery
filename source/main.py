5ef235c4f49ee2f952b4d0bca3386642a0177248
CodyPacker <cody.packer@valpo.edu>
2019-05-31 14:00:22 -0500

    Switched from PyPDF2 to PDFMiner, create output of full class csvs and discovered relavant class csvs

--- source/main.py
+++ source/main.py
@@ -4,17 +4,26 @@ from RD import findRelevant
 import json
 import csv
 
+#PDF Miner Initializations
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+import io
 
-
-
+resource_manager = PDFResourceManager()
+fake_file_handle = io.StringIO()
+converter = TextConverter(resource_manager, fake_file_handle)
+page_interpreter = PDFPageInterpreter(resource_manager, converter)
+#End PDF Miner Intitialization
 d = {}
 testing = False #testing determines if we are working in the testPDF or the fill pdf
 school = "Valpo" #school is the filename of the pdf we want, make sure its in the directory you are working in
 
 filePath = "../%sPDFs/%s.pdf" % (("test" if testing else "full"), school)
 
-pdfFileObj = open(filePath, 'rb')
-doc = PyPDF2.PdfFileReader(pdfFileObj)
+#pdfFileObj = open(filePath, 'rb')
+#doc = PyPDF2.PdfFileReader(pdfFileObj)
 
 """
 @terry
@@ -24,25 +33,38 @@ Can you make it so that we just call parse() on a pdf object and it spits out
 a dict with all the classes.
 
 """
+with open(filePath, 'rb') as fh:
+    for page in PDFPage.get_pages(fh,
+                                    caching=True,
+                                    check_extractable=True):
+        page_interpreter.process_page(page)
 
-for i in doc.pages: #for each page in the document
+    text = fake_file_handle.getvalue()
+    converter.close()
+    fake_file_handle.close()
+newClasses = parse(text,"[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}")
+#for i in doc.pages: #for each page in the document
     #parse out the classes found
-    newClasses = parse(i.extractText(),"[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}")
+#    newClasses = parse(i.extractText(),"[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}")
 
     #if the class is in the dict, append the class,
     #otherwise just create the class
-    for classID in newClasses:
-        if classID in d:
-            d[classID] += newClasses[classID]
-        else:
-            d[classID] = newClasses[classID]
-pdfFileObj.close()
+for classID in newClasses:
+    if classID in d:
+        d[classID] += newClasses[classID]
+    else:
+        d[classID] = newClasses[classID]
+#pdfFileObj.close()
 
 
 #Uncomment this for reduced csv of relavant
-#x = findRelevant(d)
-with open("../output/output%s.csv" % school, "w") as f:
+x = findRelevant(d)
+with open("../output/output%s.csv" % school, "w", encoding='utf8', errors='ignore') as f:
     f.write('ClassID,Desc\n')
     #replace d with x if wanting reduce format
-    for key in d:
-        f.write('%s,"%s"\n'%(key,d[key]))
+    for key in x:
+        try:
+            f.write('%s,"%s"\n'%(key,x[key]))
+        except:
+            print(key)
+""
--- source/parse.py
+++ source/parse.py
@@ -4,30 +4,31 @@ import csv      #CSV
 #parse is a function that, given a *string* of text, will pull out the class descriptions
 def parse(text, regex):
     #clear the new lines
+    #print(text)
     text = text.replace("\n","")
-    
+
     #create a list of match objects that each contain the indices of the matches
     matches = list(re.finditer(regex, text))
 
     result  = {}
-    
+
     #computing the length of matches here for that sweet optimization
     lengthMatches = len(matches)
-    
+
     #loop through the match objects, enumerate makes it so the index is available
     for i,m in enumerate(matches):
-            
+
             #classId is set too the current match's string
             classID = m.group(0)
             #the start of the current class's description starts with the END of the ClassID
             startDesc = m.end()
-            
+
             #if we are not on the last item,
             #the end of the description will be at the start of the next ClassID found
             #otherwise the description ends at the end of the text
-            if i < lengthMatches-1: 
+            if i < lengthMatches-1:
                 endDesc = matches[i+1].start()
-            else: 
+            else:
                 endDesc = len(text)-1
             """
             @terry, as you can see this check is does twice, once on the current page
@@ -47,4 +48,3 @@ def parse(text, regex):
 
 
 #print(parse(text,"[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}"))
-
