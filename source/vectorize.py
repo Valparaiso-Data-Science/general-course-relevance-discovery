e427a2722bb82d92a8952863bb7af7b4ac7a33bc
Cody Packer <cody.packer@valpo.edu>
2019-06-05 10:23:29 -0500

    Cleaner vectorization.py

--- source/vectorize.py
+++ source/vectorize.py
@@ -1,26 +1,34 @@
 import csv
 import os
 import numpy as np
+import sys
+#np.set_printoptions(threshold=sys.maxsize)
 
-classes = []
-names = []
-#Create list of all class descriptions
+#Grab vocab into a list
+bokVocab = [line.rstrip('\n').lower() for line in open('../bok.txt')]
+desc = []
+courseID = []
+
+#Create list of all class descriptions and course IDs
 with open('../output/outputValpo.csv', newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     for column in reader:
-        classes.append(column[0]+column[1])
-        names.append(column[0])
-#Vectorize
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-vectorizer = CountVectorizer(stop_words='english')
+        desc.append(column[0]+column[1])
+        courseID.append(column[0])
 
+#Vectorize using bok.txt
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+vectorizer = CountVectorizer( vocabulary=bokVocab)
+vectors = vectorizer.fit_transform(desc).toarray()
+vocab = np.array(vectorizer.get_feature_names())
 count = 0
-for x in classes:
-    #List of wordcount
-    vectors = vectorizer.fit_transform(x.split()).todense()
-    #List of unique words
-    vocab = np.array(vectorizer.get_feature_names())
-    #print(vectors.shape)
-    print(names[count])
-    print(vocab[0:9742],"\n")
-    count = count + 1
+classIndex = 0
+
+#Print all "relevant" courses
+for x in vectors:
+    if np.sum(line) > 0:
+        print(courseID[classIndex])
+        print(x)
+        count = count+1
+    classIndex = classIndex + 1
+print(count)
