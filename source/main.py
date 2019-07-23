3dfe97afb6b02127c2092060658907085481dc40
Nate <nathan.randle@valpo.edu>
2019-07-23 16:20:36 -0500

    Changing how data is fed to the tree
    Was counts, now it is just the presence of the word (IE binary)

--- source/main.py
+++ source/main.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree,metrics
 from sklearn.tree.export import export_text
+from sklearn.tree import export_graphviz
 
 
 
@@ -27,26 +28,21 @@ labeled_df = labelTargetsdf(pruned_df)
 print("\tfound targets")
 
 #%%
-import matplotlib.pyplot as plt
-features = labeled_df.drop("curricula relevance",axis = 1)
+features = labeled_df.drop("curricula relevance",axis = 1).astype("bool")
 labels = labeled_df["curricula relevance"]
 
-
+print("Splitting Data")
 feature_train, feature_test, answer_train, answer_test = train_test_split(features,
                                                                           labels,
                                                                           test_size=0.2)
-
-dTree = decisionTree(feature_train,answer_train,10)
-
+print("training tree")
+dTree = decisionTree(feature_train,answer_train,7)
 test_set_prediction = dTree.predict(feature_test)
+
 print("Accuracy:",metrics.accuracy_score(answer_test, test_set_prediction))
-    
+
 graph = export_text(dTree,feature_names=list(features.columns))
 print(graph)
 
-
-plot = tree.plot_tree(dTree,feature_names=list(features.columns),fontsize=10)
-
-plot.set_size_inches(15, 10)
 
 
