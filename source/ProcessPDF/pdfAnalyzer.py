8f486a3d2e1fee9ab43d80901d3d08bac5b9251b
Terry <terry.wade@valpo.edu>
2019-07-25 21:39:09 -0500

    Now parse Brown PDF perfectly

--- source/ProcessPDF/pdfAnalyzer.py
+++ source/ProcessPDF/pdfAnalyzer.py
@@ -1,54 +1,18 @@
-import io
-import re
-import fileinput
-import csv
-import pandas
+import xml.etree.ElementTree as ET
 import numpy as np
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
-from pdfminer.layout import LAParams
-from pdfminer.converter import PDFPageAggregator
-import sys
-from pdfminer.layout import LTTextBoxHorizontal
+import pandas as pd
 
-def extractTextFromPDF(pdfPath):
-    classPattern = re.compile(r'(^[A-Z]{2,5} [0-9]{2,5}.*\n$)')
-    document = open(pdfPath, 'rb')
-    rsrcmgr = PDFResourceManager()
-    laparams = LAParams()
-    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    courses = []
-    descriptions = []
-    for page in PDFPage.get_pages(document):
-        interpreter.process_page(page)
-        layout = device.get_result()
-        for element in layout:
-            if(isinstance(element, LTTextBoxHorizontal)):
-                for part in element:
-                    if(classPattern.match(part.get_text())):
-                        courses.append(part.get_text())
-    print(len(courses), courses)
+def extractTextFromXML(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    test = root.findall("Div")
+    df = pd.DataFrame(columns = ["Section", "Description"])
+    for element in test:
+        Ptags = element.findall("P")
+        if(len(Ptags) > 2):
+            df = df.append({"Section": Ptags[0].text, "Description": Ptags[1].text}, ignore_index=True)
+    df.to_csv("./output.csv")
+    print(df)
 
-                
-    #         if(isinstance(element, LTTextBoxHorizontal)):
-    #             text = element.get_text()
-    #             text = text.replace("\n", " ")
-    #             if(classPattern.match(text)):
-    #                 courses.append(text)
-    #             else:
-    #                 if(len(text) > 50):
-    #                     descriptions.append(text)
-    # df = pandas.DataFrame({"Course": courses, "Description": descriptions})
-    # df.to_csv("./output.csv")
-                
-                
-        
-    # for page in PDFPage.get_pages(document, caching = True, check_extractable = True):
-    #     interpreter.process_page(page)
-    #     layout = device.get_result()
-    #     for element in layout:
-    #         print(type(element))
 
-extractTextFromPDF(sys.argv[1])
+extractTextFromXML("C:/Users/terra/Documents/Python/DataProject/source/ProcessPDF/textXML/BrownCourses.xml")
