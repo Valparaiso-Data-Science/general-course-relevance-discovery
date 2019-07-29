afc97971388a29fb4f856d734cc9db22f0a9c78b
Terry <terry.wade@valpo.edu>
2019-07-29 12:44:40 -0500

    Add Purdue parsing

--- source/ProcessPDF/pdfAnalyzer.py
+++ source/ProcessPDF/pdfAnalyzer.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import sys
 
-def extractTextFromXML(filepath):
+def extractTextFromXMLBrown(filepath):
     tree = ET.parse(filepath)
     root = tree.getroot()
     test = root.findall("Div")
@@ -13,4 +13,13 @@ def extractTextFromXML(filepath):
         if(len(Ptags) > 2):
             df = df.append({"Section": Ptags[0].text, "Description": Ptags[1].text}, ignore_index=True)
     df.to_csv("./output.csv")
-    print(df)
+
+def extractTextFromXMLPurdue(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    test = root.findall("Part/Div")
+    df = pd.DataFrame(columns = ["Section", "Description"])
+    for element in test:
+        Ptags = element.findall("P")
+        df = df.append({"Section": Ptags[0].text, "Description": Ptags[1].text}, ignore_index=True)
+    df.to_csv("./output.csv")
