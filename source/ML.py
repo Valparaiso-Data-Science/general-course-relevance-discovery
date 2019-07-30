fdd94966a9dd7f03a889e2e64ffb42888bd669d0
Cody Packer <cpacker@valpo.edu>
2019-07-30 16:02:44 -0500

    Memory reduced to work on 3 pdfs :

--- source/ML.py
+++ source/ML.py
@@ -6,23 +6,25 @@ Created on Mon Jul 22 13:50:39 2019
 """
 from sklearn.model_selection import train_test_split
 from sklearn import tree,metrics
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import export_graphviz
-from sklearn.externals.six import StringIO  
-from IPython.display import Image  
+from sklearn.externals.six import StringIO
+from IPython.display import Image
 import pydotplus
 import graphviz
 
 
-def decisionTree(feature_train,answer_train,depth):  
-          
-    
-    
-    treeClassifier = tree.DecisionTreeClassifier(max_depth=depth)    
-    treeClassifier = treeClassifier.fit(feature_train,answer_train)    
-    
+def decisionTree(feature_train,answer_train,depth):
+
+    #forestClassifier = RandomForestClassifier(n_estimators=depth)
+#    forestClassifier.fit(feature_train,answer_train)
+
+    treeClassifier = tree.DecisionTreeClassifier(max_depth=depth)
+    treeClassifier = treeClassifier.fit(feature_train,answer_train)
+
     return treeClassifier
-    
+
 def visTree(dTree):
-    dot_data = tree.export_graphviz(dTree, out_file=None) 
-    graph = graphviz.Source(dot_data) 
+    dot_data = tree.export_graphviz(dTree, out_file=None)
+    graph = graphviz.Source(dot_data)
     return graph
--- source/main.py
+++ source/main.py
@@ -15,17 +15,20 @@ from sklearn.tree import export_graphviz
 pd.options.display.max_columns = 14
 pd.set_option('display.width', 600)
 
-courses_dict = parseDirectory("../tempPDFs/")
+courses_dict = parseDirectory("../fullPDFs/")
 courses_df = pd.DataFrame(courses_dict,index=[0,1]).transpose()
 print("\tfound courses")
 cleaned_df = cleanData(courses_df)
 print("\tcleaned")
+cleaned_df.to_csv("words.csv")
 vect_df = vectorizer(cleaned_df)
 print("\tvect")
 pruned_df = cleanVectorizer(vect_df)
 print("\tpruned")
 labeled_df = labelTargetsdf(pruned_df)
 print("\tfound targets")
+
+labeled_df.to_csv("Answer.csv")
 
 #%%
 features = labeled_df.drop("curricula relevance",axis = 1).astype("bool")
@@ -36,13 +39,10 @@ feature_train, feature_test, answer_train, answer_test = train_test_split(featur
                                                                           labels,
                                                                           test_size=0.2)
 print("training tree")
-dTree = decisionTree(feature_train,answer_train,7)
+dTree = decisionTree(feature_train,answer_train,10)
 test_set_prediction = dTree.predict(feature_test)
 
 print("Accuracy:",metrics.accuracy_score(answer_test, test_set_prediction))
 
 graph = export_text(dTree,feature_names=list(features.columns))
 print(graph)
-
-
-
