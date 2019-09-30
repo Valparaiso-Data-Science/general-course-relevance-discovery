dee2424485532e410b28620fdf959c7f97020862
Cody Packer <cody.packer@valpo.edu>
2019-09-30 11:31:41 -0500

    Dataframe format of XML parsing

--- source/parse.py
+++ source/parse.py
@@ -137,22 +137,28 @@ def extractTextFromXMLPurdue(filepath):
         df = df.append({"Section": Ptags[0].text, "Description": Ptags[1].text}, ignore_index=True)
     return df
 
+courseID = []
+descriptions = []
 def parseXML(filepath):
+    #Get xml into a variable to work on
     tree = ET.parse(filepath)
     root = tree.getroot()
-    file = open("xmlformat.txt","w")
-    print("Final: ", recursive(root, 20))
+    recursive(root)
+    courses_df = pd.DataFrame({'CourseID':courseID, 'Descriptions':descriptions, 'School': os.path.basename(filepath)})
+    print(courses_df)
 
-def recursive(text, num):
-    if (num == 0):
-        return text.text
-    else:
-        try:
-            if re.match("[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}",text[0].text) is not None and len(text[0].text)>20 and text[1].text is not None:
-                print("Course: ", text[0].text)
-                print("Desc: ", text[1].text)
-            for x in list(text):
-                recursive(x,num-1)
-        except:
-            pass
+def recursive(text):
+    try:
+        #Check if current tag holds the course ID
+        if re.match("[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}",text[0].text) is not None and len(text[0].text)>20 and text[1].text is not None:
+            #print("Course: ", text[0].text)
+            #print("Desc: ", text[1].text)
+            #courses_df = df.append({'CourseID':text[0].text,'Description':text[1].text},ignore_index=True)
+            courseID.append(text[0].text)
+            descriptions.append(text[1].text)
+        #If there are lower levels (nested tags), follow down to the bottom tag
+        for subLevel in list(text):
+            recursive(subLevel)
+    except:
+        pass
         
