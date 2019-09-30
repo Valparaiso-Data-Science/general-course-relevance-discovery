7534cbf9ec75ee13f4ccbeb5808641ecea132c1a
Cody Packer <cody.packer@valpo.edu>
2019-09-30 13:55:51 -0500

    main.py file cleanup

--- source/main.py
+++ source/main.py
@@ -1,4 +1,4 @@
-from parse import parseDirectory, extractTextFromXMLBrown, extractTextFromXMLValpo, extractTextFromXMLPurdue, parseXML
+from parse import parseXML
 from vectorize import cleanData, vectorizer, cleanVectorizer, labelTargetsdf
 from ML import decisionTree,visTree
 
@@ -15,45 +15,39 @@ pd.set_option('display.width', 600)
 
 for filename in os.listdir('../fullPDFs/'):
     parseXML("../fullPDFs/"+filename).to_csv("../courses/"+filename.replace("xml","csv"))
-# young_df = parseXML("../fullPDFs/Youngstown.xml")
-# cornell_df = parseXML("../fullPDFs/2011Cornell.xml")
-# smith_df = parseXML("../fullPDFs/Smith.xml")
-# brown_df = parseXML("../fullPDFs/BrownCourses.xml")
-# purdue_df = parseXML("../fullPDFs/Purdue.xml")
 
-pd.concat([young_df,cornell_df,smith_df,brown_df,purdue_df]).to_csv("output.csv")
-courses_df = pd.concat([brown_df,purdue_df])
-
-print("\tfound courses")
-cleaned_df = cleanData(courses_df)
-print("\tcleaned")
-vect_df = vectorizer(cleaned_df)
-print("\tvect")
-pruned_df = cleanVectorizer(vect_df)
-print("\tpruned")
-labeled_df = labelTargetsdf(pruned_df)
-print("\tfound targets")
-#%%
-features = labeled_df.drop("curricula relevance",axis = 1).astype("bool")
-labels = labeled_df["curricula relevance"]
-
-print("Splitting Data")
-feature_train, feature_test, answer_train, answer_test = train_test_split(features,
-                                                                          labels,
-                                                                          test_size=0.2)
-print("training tree")
-dTree = decisionTree(feature_train,answer_train,20)
-test_set_prediction = dTree.predict(feature_test)
-
-print("Accuracy:",metrics.accuracy_score(answer_test, test_set_prediction))
-
-graph = export_text(dTree,feature_names=list(features.columns))
-print(graph)
-print(export_graphviz(dTree,feature_names=list(features.columns),filled=True,impurity=False,label='root'))
-
-
-mlaoutput = pd.DataFrame(test_set_prediction,columns=["machineAlg"])
-
-answer_test.append(mlaoutput).to_csv("answer.csv")
-answer_test['Predicted'] = pd.Series(test_set_prediction)
-
+#pd.concat([young_df,cornell_df,smith_df,brown_df,purdue_df]).to_csv("output.csv")
+# 
+# print("\tfound courses")
+# cleaned_df = cleanData(courses_df)
+# print("\tcleaned")
+# vect_df = vectorizer(cleaned_df)
+# print("\tvect")
+# pruned_df = cleanVectorizer(vect_df)
+# print("\tpruned")
+# labeled_df = labelTargetsdf(pruned_df)
+# print("\tfound targets")
+# #%%
+# features = labeled_df.drop("curricula relevance",axis = 1).astype("bool")
+# labels = labeled_df["curricula relevance"]
+# 
+# print("Splitting Data")
+# feature_train, feature_test, answer_train, answer_test = train_test_split(features,
+#                                                                           labels,
+#                                                                           test_size=0.2)
+# print("training tree")
+# dTree = decisionTree(feature_train,answer_train,20)
+# test_set_prediction = dTree.predict(feature_test)
+# 
+# print("Accuracy:",metrics.accuracy_score(answer_test, test_set_prediction))
+# 
+# graph = export_text(dTree,feature_names=list(features.columns))
+# print(graph)
+# print(export_graphviz(dTree,feature_names=list(features.columns),filled=True,impurity=False,label='root'))
+# 
+# 
+# mlaoutput = pd.DataFrame(test_set_prediction,columns=["machineAlg"])
+# 
+# answer_test.append(mlaoutput).to_csv("answer.csv")
+# answer_test['Predicted'] = pd.Series(test_set_prediction)
+# 
--- source/parse.py
+++ source/parse.py
@@ -21,9 +21,6 @@ def recursive(text):
     try:
         #Check if current tag holds the course ID
         if re.match("[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}",text[0].text) is not None and len(text[0].text)>20 and text[1].text is not None:
-            print("Course: ", text[0].text,"\n")
-            print("Desc: ", text[1].text,"\n")
-            print("Try3: ", text[2].text,"\n")
             #courses_df = df.append({'CourseID':text[0].text,'Description':text[1].text},ignore_index=True)
             courseID.append(text[0].text)
             descriptions.append(text[1].text)
