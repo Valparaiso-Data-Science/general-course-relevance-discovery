2e4c6b30144e9b7523b343811851e6b7e8773be7
Cody Packer <cody.packer@valpo.edu>
2020-01-22 10:28:28 -0600

    Fixed xmls getting errored out on run

--- source/parse.py
+++ source/parse.py
@@ -2,37 +2,49 @@ import re
 import xml.etree.ElementTree as ET
 import pandas as pd
 import os
+from lxml import etree
 
 def parseXML(filepath, courseTag, descTag, descTagsFromID):
     # Get xml (tree) into a list (stack) and find courses (courseID & descriptions)
-    tree = ET.parse(filepath)
+    parser = etree.XMLParser(recover=True)
+    tree = ET.parse(filepath, parser=parser)
     text = tree.getroot()
     courseID = []
     descriptions = []
     stack = []
 
     #Convert xml text into stack list
+    #Probably here: if file is .xml do this, if file is .pkl skip this and set stack equal to that
     for subLevel in text:
         recursive(subLevel, stack)
 
     counter = 0
     for xml in stack:
         if xml.text is not None:
-            #Check if description is greater than 50char? (to remove more tables)
+            # if re.match("[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}", xml.text) is not None:
+            #     print(xml.tag, xml.text)
+            #     print("1:",stack[counter+1].tag, stack[counter+1].text)
+            #     print("2:",stack[counter+2].tag, stack[counter+2].text)
+            #     print("3:", stack[counter + 3].tag, stack[counter + 3].text)
+            #     print("")
+
+            #Check not empty, check course tag is proper, check desc tag is proper, check desc word count
             if re.match("[A-Z]{2,5}\s[0-9]{3,4}[A-Z]{0,1}", xml.text) is not None and \
                     (xml.tag == courseTag) and \
                     (stack[counter + descTagsFromID].tag == descTag) and \
-                    len(stack[counter + descTagsFromID].text) > 30:
-
-                courseID.append(xml.tag + " " + xml.text)
-                descriptions.append(stack[counter + descTagsFromID].tag + " " + stack[counter + descTagsFromID].text)
+                    len(re.findall(r'\w+', stack[counter + descTagsFromID].text)) > 15:
+                #
+                # print(xml.tag, xml.text)
+                # print("1:",stack[counter+1].tag, stack[counter+1].text)
+                # print("")
+                courseID.append(xml.text)
+                descriptions.append(stack[counter + descTagsFromID].text)
 
                 # print(xml.tag, xml.text)
                 # print(stack[counter + descTagsFromID].tag, stack[counter + descTagsFromID].text)
         counter += 1
 
     courses_df = pd.DataFrame({'School': os.path.basename(filepath.replace(".xml", "")), 'CourseID': courseID, 'Descriptions': descriptions})
-
     return (courses_df)
 
 
