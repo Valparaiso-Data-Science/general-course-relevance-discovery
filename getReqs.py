328c985a2df5430c17682611fe3bc0214b571e22
Ethan <ethan.hawk@valpo.edu>
2020-06-09 11:15:13 -0500

    getReqs now tells the user to either install enchant through their package manager if they are on linux, installs it via brew if they are on macos, and alerts windows users that they will need to download it

--- getReqs.py
+++ getReqs.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 import os
+from sys import platform
 os.system("pip install -r requirements.txt")
 os.system("python -m spacy download en")
-#os.system("brew install enchant") # if a mac user and the have homebrew installed
+if platform == "linux":
+    print("------------------------------------------------------------------------------------------")
+    print("Use your package manager to install 'enchant'")
+    print("------------------------------------------------------------------------------------------")
+elif platform == "darwin":
+    os.system("brew install enchant")
+elif platform == "win32":
+    print("------------------------------------------------------------------------------------------")
+    print("You need to install 'enchant' for the wordninja parsing to work correctly")
+    print("------------------------------------------------------------------------------------------")
