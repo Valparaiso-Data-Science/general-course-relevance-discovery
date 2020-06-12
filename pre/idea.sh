395ac9fe5cd0959ff7296d516861d7b6387c89ca
Ethan <ethan.hawk@valpo.edu>
2020-06-12 16:31:49 -0500

    replaced / with ~ to improve readability of sed command

--- pre/idea.sh
+++ pre/idea.sh
@@ -13,7 +13,7 @@ id_desc_sep="</P><P>" # this separator will be replaced with a comma
 #grep -E "^<P>[A-Z]{2,} [0-9]{2,}" $file # Course ID regex
 #cmd currently requires gnu sed (wont work on bsd or macos ootb)
 #the gnu-ism is now replaced (sed's -r option is now -E), so in theory it should work on POSIX systems
-cat "$file" | tr -d '\n' | sed -E 's/(<P>[A-Z]{2,} [0-9]{2,})/\n\1/g'
+cat "$file" | tr -d '\n' | sed -E 's~(<P>[A-Z]{2,} [0-9]{2,})~\n\1~g'
 
 # other commands I was investigating
 #| sed 's/\(<P>[A-Z]{2,} [0-9]{2,}\)/\n\1/g'
