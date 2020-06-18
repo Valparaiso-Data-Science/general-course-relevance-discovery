3a6aed5cff404826956a0e8396c4b6a821ed8bd2
Syd Shearer <65626074+sshearer2000@users.noreply.github.com>
2020-06-18 16:59:46 -0500

    Update tfidf.py
    Add overall tf-idf calculation

--- analysis/tfidf.py
+++ analysis/tfidf.py
@@ -1,14 +1,9 @@
-"""##TF-IDF Function
-IN:
-- dictionary: dictionary being accessed
-- schools: list of school names
-
-No OUT, creates csv file for each category in categories
-"""
-
 def tfidf(dictionary, schools):
   nlp = spacy.load("en")
   answers = dictionary
+  overall_denom = 0
+  #TF-IDF FOR EACH INDIVIDUAL SCHOOL
+  #TERM = WORD/BIGRAM, DOCUMENT = DESCRIPTION, CORPUS = ALL DESCRIPTIONS FROM ONE SCHOOL
   corpus = []
   #for school in schools:
     #doc_string = ' '.join(dictionary[school])
@@ -42,8 +37,8 @@ def tfidf(dictionary, schools):
     while i < 10:
       bigram = str(list_bigram[i]).split()
       f_word = str(bigram[0])
-      #s_word = str(bigram[1])
-      regex = re.escape(f_word) #+ r" " + re.escape(s_word)
+      #s_word = str(bigram[1]) -- add if bigram
+      regex = re.escape(f_word) #+ r" " + re.escape(s_word) -- add if bigram
       count = 0 
       for doc in corpus:
         iterator = finditer(regex, doc)
@@ -54,11 +49,65 @@ def tfidf(dictionary, schools):
       i+=1
     words['frequency'] = freq_list
     denom = len(dictionary[school])
+    overall_denom += denom
     words ['overall'] = str(denom)
-    words['category'] = school
-    words = words[['category','term','frequency','overall','rank']]
+    words = words[['term','frequency','overall','rank']]
     print('\n',words)
     print("\n---------------------------\n")
+
+
+
+#TF-IDF FOR ALL SCHOOLS COMBINED 
+#TERMS = WORD/BIGRAM, DOCUMENT = EACH SCHOOL'S CATALOG, CORPUS = ALL CATALOGS
+  print("All Schools: -----------------")
+  corpus = []
+  for school in schools:
+    doc_string = ' '.join(dictionary[school])
+    corpus.append(doc_string)
+
+  #code from https://www.geeksforgeeks.org/tf-idf-for-bigrams-trigrams/
+  #GETTING BIGRAMS
+  vectorizer = CountVectorizer(ngram_range = (1,1)) 
+  X1 = vectorizer.fit_transform(corpus)  
+  features = (vectorizer.get_feature_names()) 
+    
+  # Applying TFIDF 
+  vectorizer = TfidfVectorizer() 
+  vectors = vectorizer.fit_transform(corpus) 
+  scores = (vectors.toarray()) 
+  # Getting top ranking features 
+  sums = vectors.sum(axis = 0) 
+  data1 = [] 
+  for col, term in enumerate(features): 
+      data1.append( (term, sums[0,col] )) 
+  ranking = pd.DataFrame(data1, columns = ['term','rank']) 
+  words = (ranking.sort_values('rank', ascending = False)) 
+  words = words[:10]
+
+  #Getting frequency counts
+  freq_list = []
+  list_bigram = list(words['term'])
+  i = 0
+  while i < 10:
+    bigram = str(list_bigram[i]).split()
+    f_word = str(bigram[0])
+    #s_word = str(bigram[1])
+    regex = re.escape(f_word) #+ r" " + re.escape(s_word)
+    count = 0 
+    for doc in corpus:
+      iterator = finditer(regex, doc)
+      for match in iterator:
+        count+=1
+    app = str(count)
+    freq_list.append(app)
+    i+=1
+  words['frequency'] = freq_list
+  denom = overall_denom
+  words ['overall'] = str(denom)
+  words = words[['term','frequency','overall','rank']]
+  print('\n',words)
+  print("\n---------------------------\n")
+
 '''
     #saving dataframe to csv
     path_name = 'TFIDF_' + school +'.csv'
