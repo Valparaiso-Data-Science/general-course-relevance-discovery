f4b6284d22a5379363d011ff80c01356997946c1
Syd Shearer <65626074+sshearer2000@users.noreply.github.com>
2020-06-18 16:27:20 -0500

    Create get_df_name.py

---
+++ analysis/get_df_name.py
@@ -0,0 +1,11 @@
+"""##Retrieve Dataframe Name Function
+IN:
+- df: dataframe
+
+OUT:
+- name: name of dataframe
+"""
+
+def get_df_name(df):
+    name =[x for x in globals() if globals()[x] is df][0]
+    return name
