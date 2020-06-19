e8981b1481f04cbf567acd847b518c7cc092aa2c
sshearer2000 <65626074+sshearer2000@users.noreply.github.com>
2020-06-19 12:44:28 -0500

    update import statements

--- analysis/main.py
+++ analysis/main.py
@@ -7,10 +7,10 @@ import pandas as pd
 #import numpy as np
 import nltk
 import spacy
-#nltk.download('punkt')
+nltk.download('punkt')
 nltk.download('stopwords')
-#nltk.download('wordnet')
-#nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 #import string
 #import re
 #from re import *
