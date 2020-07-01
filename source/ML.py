3b89ed18df00c1c8acb1e9e4a265a92b55286353
frankie <frankie.vazquez@valpo.edu>
2020-07-01 13:09:15 -0500

    Moved the random forest code to ML.py

--- source/ML.py
+++ source/ML.py
@@ -4,7 +4,7 @@ Created on Mon Jul 22 13:50:39 2019
 
 @author: nrandle
 """
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn import tree,metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import export_graphviz
@@ -13,7 +13,37 @@ from IPython.display import Image
 import pydotplus
 import graphviz
 
-
+def randForest(features,labels):
+    skf = StratifiedKFold(n_splits=10,shuffle=True, random_state = 19)
+    # skf.split(features,labels)
+    # errors = []
+    accs = [0]*10
+    count=0
+    for train_index, test_index in skf.split(features, labels):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        # X_train = [features.iloc[i] for i in train_index]
+        # X_test=[features.iloc[i] for i in test_index]
+        X_train = features.iloc[train_index]
+        X_test = features.iloc[test_index]
+        y_train = labels.iloc[train_index]
+        y_test = labels.iloc[test_index]
+        rf = RandomForestClassifier(n_estimators = 1000, random_state = 42)
+        rf.fit(X_train, y_train)
+        preds = rf.predict(X_test)
+        #errors.append(round(np.mean(abs(preds - y_test)),2))
+        i=0
+        for pred in preds:
+            if pred == y_test[i]:
+                accs[count]+=1
+            i +=1
+        accs[count] = (accs[count]/len(preds))*100
+        count += 1
+        print('R^2: ' + str(rf.score(X_test,y_test)))
+    count = 0
+    for acc in accs:
+        #print("Mean Absolute Error for Forest #" + str(count) + ": " + str(error) + ' degrees.')
+        print("Accuracy for Forest #"+ str(count)+ ": " + str(acc) + " percent")
+        count += 1
 def decisionTree(feature_train,answer_train,depth):
 
     #forestClassifier = RandomForestClassifier(n_estimators=depth)
