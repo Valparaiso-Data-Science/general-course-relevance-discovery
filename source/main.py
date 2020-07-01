f3a8caa9590c04794b3792f45efcd2b103639d85
slioutik <55065869+slioutik@users.noreply.github.com>
2020-07-01 12:46:51 -0500

    testing undersampling to help imbalanced dataset for machine learning

--- source/main.py
+++ source/main.py
@@ -1,5 +1,5 @@
 # files in the current directory
-from parse import parseXML, fixTags, trimFile
+from parse import parseXML, fixTags
 #from topicModel import plot_10_most_common_words, listofDSCourse
 from vectorize import newClean, vectorizer, cleanVectorizer, labelTargetsdf
 from ML import decisionTree,visTree
@@ -7,7 +7,8 @@ from reintroduce_spaces import reintroduce_spaces
 from xml_fix_utils import correct_ampersands, ignore_bad_chars
 
 #libraries
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.tree import export_text
 from sklearn.tree import export_graphviz
@@ -46,8 +47,9 @@ def prepare():
         os.mkdir('../temp_data/superTrimmedPDFs')
     except FileExistsError:
         print("../temp_data/superTrimmedPDFs already exists. Clearing all files in it.")
+
         # clear folder of previous files
-        for file in Bar("Cleaning supertrimmmed...").iter(os.listdir('../temp_data/superTrimmedPDFs')):
+        for file in os.listdir('../temp_data/superTrimmedPDFs'):
             os.unlink('../temp_data/superTrimmedPDFs/' + file)
     try:
         os.mkdir('../courses')
@@ -55,13 +57,13 @@ def prepare():
         print("../courses already exists. Clearing all files in it.")
 
         # clear folder of previous files
-        for file in Bar("Cleaning courses...").iter(os.listdir('../courses')):
+        for file in os.listdir('../courses'):
             os.unlink('../courses/' + file)
 prepare()
 
 # make trimmed files
 try:
-    os.system("bash ../pre/fileTrimmer.sh ../Catalogs.csv ../fullPDFs " + trimmed_dir)
+    os.system("bash ../pre/fileTrimmer.sh ../Catalogs.csv ../fullPDFs ../temp_data/TRIMMED")
 except:
     print("Filetrimming step failed, we'll get em next time.")
     quit()
@@ -77,26 +79,63 @@ def makeCSV(filename):
     global supertrimmed_dir
 
     #Checks if we are looking at a college we know needs WordNinja
-    wn_colleges = ['Brown', '2011Cornell', 'Carlow', 'Caldwell', 'Denison', 'Pittsburgh'] # 'Youngstown']
-
+    wn_colleges = ['Brown', '2011Cornell', 'Carlow', 'Caldwell', 'Denison', 'Pittsburgh']
+    
     for college in wn_colleges:
         if re.match(college,filename) is not None:
             needsWN = True
             break
         else:
             needsWN = False
-
     #Checks if the college needs Word Ninja
     if needsWN:
-        deletable = filename
+        # container of intermediary data files for potential deletion
+        deletable_filenames = []
 
-        # reintroduce spaces and reassign `filename` to cleaned file
-        filename = reintroduce_spaces(supertrimmed_dir + "/" + filename)
-        filename = filename[filename.rfind("/") + 1:]  # chop off the directory path, only leave name filename
+        #Pass the super trimmed XML into Word Ninja
+        try:
+            # save current name for potential deletion later
+            deletable = filename
 
+            # reintroduce spaces and reassign `filename` to cleaned file
+            filename = reintroduce_spaces(supertrimmed_dir + "/" + filename)
+            filename = filename[filename.rfind("/")+1:]  # chop off the directory path, only leave name filename
+
+            deletable_filenames.append(deletable)
+
+        except xml.etree.ElementTree.ParseError:
+            # save current name for potential deletion later
+            deletable = filename
+
+            # clean bad characters (so far only utf 65535) and reassign `filename` to cleaned file
+            filename = ignore_bad_chars(supertrimmed_dir + "/" + filename)
+            filename = filename[filename.rfind("/")+1:]  # chop off the directory path, only leave name filename
+            deletable_filenames.append(deletable)
+
+
+            # save current name for potential deletion later
+            deletable = filename
+
+            # correct bad apersands if any (replace `&` with `&amp;`) and reassign `filename` to cleaned file
+            filename = correct_ampersands(supertrimmed_dir + "/" + filename)
+            filename = filename[filename.rfind("/")+1:]  # chop off the directory path, only leave name filename
+            deletable_filenames.append(deletable)
+
+            # save current name for potential deletion later
+            deletable = filename
+
+            # correct spaces and reassign `filename` to cleaned file
+            filename = reintroduce_spaces(supertrimmed_dir + "/" + filename)
+            filename = filename[filename.rfind("/")+1:]  # chop off the directory path, only leave name filename
+            deletable_filenames.append(deletable)
+
+        #Delete the old, not Word Ninja-ed file(s)
         if not dirty:
-            print("\nNow deleting:", deletable)
-            os.unlink(supertrimmed_dir + "/" + deletable)
+            # print the whole list of deletable filenames
+            print(f'\nNow deleting: {*deletable_filenames,}')
+
+            for item in deletable_filenames:
+                os.remove(supertrimmed_dir + "/" + item)
 
     # use parseXML to find course headers and descriptions
     CSV = parseXML(supertrimmed_dir + "/" + filename, 'P', 'P', 1)
@@ -115,7 +154,7 @@ topicModel = pd.concat(df_container)
 cleaned_df = newClean(topicModel)
 print("Creating '../courses/AllSchools.csv'...")
 cleaned_df.to_csv('../courses/AllSchools.csv', encoding="utf-8-sig")
-'''
+
 #Previously untouched last semester Spring2020 from here down
 print("\tcleaned")
 vect_df = vectorizer(cleaned_df)
@@ -128,22 +167,84 @@ print("\tfound targets")
 features = labeled_df.drop("curricula relevance",axis = 1).astype("bool")
 labels = labeled_df["curricula relevance"]
 
+# testing imbalanced learn
+from imblearn.under_sampling import RandomUnderSampler
+from collections import Counter
+
+rus = RandomUnderSampler(sampling_strategy=0.75, random_state=42)
+X_res, y_res = rus.fit_resample(features, labels)
+
+print('Original dataset shape %s' % Counter(labels))
+print('Resampled dataset shape %s' % Counter(y_res))
+
+print('Sample indices:')
+print(rus.sample_indices_)
+
+'''
 print("Splitting Data")
+
+skf = StratifiedKFold(n_splits=10,shuffle=True, random_state = 19)
+# skf.split(features,labels)
+errors = []
+accs = [0]*10
+count=0
+for train_index, test_index in skf.split(features, labels):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    # X_train = [features.iloc[i] for i in train_index]
+    # X_test=[features.iloc[i] for i in test_index]
+    X_train = features.iloc[train_index]
+    X_test = features.iloc[test_index]
+    y_train = labels.iloc[train_index]
+    y_test = labels.iloc[test_index]
+    rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+    rf.fit(X_train, y_train)
+    preds = rf.predict(X_test)
+    #errors.append(round(np.mean(abs(preds - y_test)),2))
+    i=0
+    for pred in preds:
+        if pred == y_test[i]:
+            accs[count]+=1
+        i +=1
+    accs[count] = (accs[count]/len(preds))*100
+    count += 1
+    print('R^2: ' + str(rf.score(X_test,y_test)))
+count = 0
+for acc in accs:
+    #print("Mean Absolute Error for Forest #" + str(count) + ": " + str(error) + ' degrees.')
+    print("Accuracy for Forest #"+ str(count)+ ": " + str(acc) + " percent")
+    count += 1
+'''
+
+'''
 feature_train, feature_test, answer_train, answer_test = train_test_split(features, labels, test_size=0.2)
+
+rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+
+rf.fit(feature_train, answer_train)
+
+preds = rf.predict(feature_test)
+
+errors = abs(preds - answer_test)
+
+print("Mean Absolute Error: ", round(np.mean(errors), 2), 'degrees.')
+
 
 print("training tree")
 dTree = decisionTree(feature_train,answer_train,20)
 test_set_prediction = dTree.predict(feature_test)
 
-print("Accuracy:",accuracy_score(answer_test, test_set_prediction))
+
+#ADD IN CODE TO GET AND PRINT THE GOOD GOOD TREE
+dTree = rf.estimators_[5]
+#print("Accuracy:",accuracy_score(answer_test, preds))
 
 graph = export_text(dTree,feature_names=list(features.columns))
 print(graph)
 print(export_graphviz(dTree,feature_names=list(features.columns),filled=True,impurity=False,label='root'))
 
 
-mlaoutput = pd.DataFrame(test_set_prediction,columns=["machineAlg"])
+mlaoutput = pd.DataFrame(preds,columns=["machineAlg"])
 
 answer_test.append(mlaoutput).to_csv("answer.csv")
-answer_test['Predicted'] = pd.Series(test_set_prediction)
+answer_test['Predicted'] = pd.Series(preds)
 '''
