2100956b9a2a5b244ff14c74c81887898fdded97
frankie <frankie.vazquez@valpo.edu>
2020-07-09 13:28:41 -0500

    Still trying to fix Near Miss

--- source/ML.py
+++ source/ML.py
@@ -102,7 +102,7 @@ def svm(features,labels,splits):
 
 def undersample(features, labels, split=0.5):
     #rus = RandomUnderSampler(sampling_strategy=split, random_state=19)
-    nmus = NearMiss(version=3)
+    nmus = NearMiss(version=3,sampling_strategy={0:sum(labels),1:sum(labels)})
     newFeatures, newLabels = nmus.fit_resample(features, labels)
     print(len(newLabels))
     print(sum(newLabels))
