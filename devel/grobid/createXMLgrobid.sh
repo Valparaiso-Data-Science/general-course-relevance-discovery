ae69b605bf8aa5c77842d6e5da432703ac811faf
Ethan <ethan.hawk@valpo.edu>
2020-07-10 17:47:46 -0500

    updated createXMLgrobid.sh

--- devel/grobid/createXMLgrobid.sh
+++ devel/grobid/createXMLgrobid.sh
@@ -4,6 +4,11 @@ xml_out_dir="out" #obviously change
 
 mkdir -pv $xml_out_dir
 
+check_environment(){
+	docker --version || echo "Docker is not installed. Exiting..." && exit 1
+	python --version || echo "Python is not installed. Exiting..." && exit 1
+}
+
 start_docker(){
 	docker kill grobid # assumes container name is grobid
 	docker run -d -t --rm --name grobid --init -p 8080:8070 -p 8081:8071 lfoppiano/grobid:0.6.0
@@ -13,9 +18,12 @@ copy_config(){
 	cp config.json grobid-python-client/
 }
 
-
 python_client(){
 	python3 grobid-python-client/grobid-client.py --input $in_dir --output $out_dir processFulltextDocument
 }
+
+# need to figure out how to decide which catalogs to download (use the scripts in fetchpdfs)
+
+
 
 
