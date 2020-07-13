410b7397d4a42b2e16bc42849272e1ad52affb42
frankie <frankie.vazquez@valpo.edu>
2020-07-13 12:42:50 -0500

    Trying to improve time

--- source/ML.py
+++ source/ML.py
@@ -117,7 +117,8 @@ def svm(features,labels,splits):
 def undersample(features, labels, split=0.5):
     #rus = RandomUnderSampler(sampling_strategy=split, random_state=19)
     nmus = NearMiss(version=3,sampling_strategy={0:sum(labels),1:sum(labels)})
-    newLabels = nmus.fit_resample(features, labels).sample_indices_
+    nmus.fit_resample(features, labels)
+    newLabels = nmus.sample_indices_
     print(len(newLabels))
     #print(sum(newLabels))
     # print(newFeatures)
