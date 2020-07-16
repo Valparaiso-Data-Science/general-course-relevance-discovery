35936f44254391e47ce1d14bc0e8284ae7baf7eb
frankie <frankie.vazquez@valpo.edu>
2020-07-16 17:09:25 -0500

    Fixed a quick bug

--- source/main.py
+++ source/main.py
@@ -47,7 +47,7 @@ features, labels = ML.preProcess()
 #ML.stratKFold(features, labels)
 #svm(features,labels,5)
 #undersample(features, labels)
-randForest(features,labels)
+ML.randForest(features,labels)
 
 '''
 feature_train, feature_test, answer_train, answer_test = train_test_split(features, labels, test_size=0.2)
