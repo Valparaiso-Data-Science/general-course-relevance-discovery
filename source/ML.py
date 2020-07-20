94bff5ca93867dc8471e36459ebbcb5ee68a01a4
frankie <frankie.vazquez@valpo.edu>
2020-07-20 13:39:22 -0500

    Adding in old versions of randForest and UnderSample to get results

--- source/ML.py
+++ source/ML.py
@@ -226,6 +226,57 @@ def svm(features,labels,splits):
         print("Accuracy for SVM #"+ str(count)+ ": " + str(acc) + " percent")
         count += 1
 
+def OldrandForest(features,labels,splits=10):
+    '''
+    Parameters
+    ----------
+    features : list
+        Array of feature data.
+    labels : list
+        Array of labels.
+    splits : int
+        Number of splits to use in the stratified k-fold.
+    Returns
+    -------
+    None.
+    '''
+
+    skf = StratifiedKFold(n_splits=splits,shuffle=True, random_state = 19)
+    # skf.split(features,labels)
+    # errors = []
+    accs = [0]*splits
+    count=0
+    for train_index, test_index in skf.split(features, labels):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        # X_train = [features.iloc[i] for i in train_index]
+        # X_test=[features.iloc[i] for i in test_index]
+        X_train = features.iloc[train_index]
+        X_test = features.iloc[test_index]
+        y_train = labels.iloc[train_index]
+        y_test = labels.iloc[test_index]
+        X_train,y_train = undersample(X_train,y_train)
+        rf = RandomForestClassifier(n_estimators = 100, random_state = 42)
+        rf.fit(X_train, y_train)
+        preds = rf.predict(X_test)
+        #errors.append(round(np.mean(abs(preds - y_test)),2))
+        i=0
+        for pred in preds:
+            if pred == y_test[i]:
+                accs[count]+=1
+            i +=1
+        accs[count] = (accs[count]/len(preds))*100
+        count += 1
+        forAc = rf.score(X_test,y_test)
+        accs.append(forAc)
+        print('Mean Accuracy: ' + str(forAc))
+        print(confusion_matrix(y_test, preds))
+        #vizRFTrees(rf, 5, features)
+    count = 0
+    modelAc= sum(accs)/len(accs)
+    print("Average Model Accuracy: "+ str(modelAc))
+    results = open('../source/RandForestResult.txt','w')
+    r = results.write("Average Model Accuracy: "+ str(modelAc))
+    results.close()
 
 def undersample(features, labels, split=0.5):
     '''
@@ -265,6 +316,15 @@ def undersample(features, labels, split=0.5):
     #print(sum(newLabels))
     # print(newFeatures)
     return newLabels
+
+def Oldundersample(features, labels, split=0.5):
+    #rus = RandomUnderSampler(sampling_strategy=split, random_state=19)
+    nmus = NearMiss(version=3,sampling_strategy={0:sum(labels),1:sum(labels)})
+    newFeatures, newLabels = nmus.fit_resample(features, labels)
+    print(len(newLabels))
+    print(sum(newLabels))
+    print(newFeatures)
+    return newFeatures, newLabels
 
 def vizRFTrees(rf, ntree,features):
     '''
