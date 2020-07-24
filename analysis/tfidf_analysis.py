6b7675fd66b218ae29ed5d76b8ba02ebb261c3f8
Syd Shearer <65626074+sshearer2000@users.noreply.github.com>
2020-07-24 13:27:41 -0500

    Update tfidf_analysis.py
    added comments

--- analysis/tfidf_analysis.py
+++ analysis/tfidf_analysis.py
@@ -16,56 +16,56 @@ def tfidf(dictionary, schools):
   #TF-IDF FOR EACH INDIVIDUAL SCHOOL
   #TERM = WORD/BIGRAM, DOCUMENT = DESCRIPTION, CORPUS = ALL DESCRIPTIONS FROM ONE SCHOOL
   corpus = []
-  for school in schools:
+  for school in schools: #for each school
     print("School: %s -----------------" % school)
     corpus=dictionary[school]
     #code from https://www.geeksforgeeks.org/tf-idf-for-bigrams-trigrams/
     #GETTING BIGRAMS
-    vectorizer = CountVectorizer(ngram_range = (2,2))
+    vectorizer = CountVectorizer(ngram_range = (2,2)) #create bigrams
     X1 = vectorizer.fit_transform(corpus)
     features = (vectorizer.get_feature_names())
     i = len(features) - 1
-    while i >= 0:
+    while i >= 0: #remove bigrams that are the same word twice
       bi = features[i].split()
       if bi[0] == bi[1]:
         del(features[i])
       i -= 1
 
     # Applying TFIDF
-    vectorizer = TfidfVectorizer(ngram_range = (2,2))
+    vectorizer = TfidfVectorizer(ngram_range = (2,2)) #create tfidf of bigrams
     vectors = vectorizer.fit_transform(corpus)
     scores = (vectors.toarray())
     # Getting top ranking features
     sums = vectors.sum(axis = 0)
     data1 = []
     for col, term in enumerate(features):
-        data1.append( (term, sums[0,col] ))
-    ranking = pd.DataFrame(data1, columns = ['term','rank'])
-    words = (ranking.sort_values('rank', ascending = False))
-    words = words[:10]
+        data1.append( (term, sums[0,col] )) #append bigram and tfidf weight to list
+    ranking = pd.DataFrame(data1, columns = ['term','rank']) #append list to datframe
+    words = (ranking.sort_values('rank', ascending = False)) #sort by weight
+    words = words[:10] #keep only top 10 bigrams
 
     #Getting frequency counts
     freq_list = []
     list_bigram = list(words['term'])
     i = 0
-    while i < 10:
+    while i < 10: #loop through all 10 bigrams
       bigram = str(list_bigram[i]).split()
-      f_word = str(bigram[0])
-      s_word = str(bigram[1])
-      regex = re.escape(f_word) + r"\w*\s+" + re.escape(s_word)
+      f_word = str(bigram[0]) #first word in bigram
+      s_word = str(bigram[1]) #second word in bigram
+      regex = re.escape(f_word) + r"\w*\s+" + re.escape(s_word) #regex for "firstword space secondword"
       count = 0
-      for doc in corpus:
-        iterator = finditer(regex, doc)
+      for doc in corpus: #for each document
+        iterator = finditer(regex, doc) #count how many times bigram appears
         for match in iterator:
           count+=1
       app = str(count)
-      freq_list.append(app)
+      freq_list.append(app) #add count to list
       i+=1
-    words['frequency'] = freq_list
-    denom = len(dictionary[school])
+    words['frequency'] = freq_list #add list to dataframe
+    denom = len(dictionary[school]) #create denominator column
     overall_denom += denom
-    words ['overall'] = str(denom)
-    words = words[['term','frequency','overall','rank']]
+    words ['overall'] = str(denom) #add denominator to dataframe 
+    words = words[['term','frequency','overall','rank']] #reorganize dataframe
     print('\n',words)
     print("\n---------------------------\n")
 
@@ -76,6 +76,7 @@ def tfidf(dictionary, schools):
 
 #TF-IDF FOR ALL SCHOOLS COMBINED
 #TERMS = WORD/BIGRAM, DOCUMENT = EACH SCHOOL'S CATALOG, CORPUS = ALL CATALOGS
+#same process as above with differing documents/corpus
   print("All Schools: -----------------")
   corpus = []
   for school in schools:
